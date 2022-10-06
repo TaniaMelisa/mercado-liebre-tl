003e5e68374360859e878c456e16b8be72cab7e7
TaniaMelisa <taniamelisalopez@gmail.com>
2022-10-06 17:54:52 -0300

    probando si sube

---
+++ Logo.bat
@@ -0,0 +1,102 @@
+Logo
+● Lugar para poner la barra de búsqueda
+● Comprá en cuotas y sin tarjeta de crédito
+● Barra de navegación que posea las siguientes opciones:
+○ Ofertas
+○ Tiendas Oficiales
+○ Vender
+○ Ayuda
+○ Creá tu cuenta
+○ Ingresá
+○ Mis compras
+
+                Main (Productos)
+Sección: Basado en tu última visita
+imagen de la cafetera
+$6.770
+40% OFF
+Cafetera Moulinex
+imagen de MacBook
+$230.000
+20% off
+MacBook Pro 2019
+imagen del Samsung Galaxy
+$70.500
+10% off
+Samsung Galaxy S10
+imagen del SmartTv
+$23.200
+5% off
+SmartTv Samsung 43"
+● Sección de Ofertas
+imagen de la cafetera
+$6.770
+40% off
+Cafetera Moulinex
+imagen de MacBook
+$230.000
+20% off
+MacBook Pro 2019
+imagen Samsung
+$70.500
+10% off
+Samsung Galaxy S10
+imagen SmarTv
+$23.200
+5% off
+SmartTv Samsung 43"
+
+          Footer (Pie de Página)
+Tendrá las siguientes secciones:
+● Pagá con tarjeta o en efectivo
+Con Mercado Liebre Cash, tenés cuotas sin interés con tarjeta o efectivo en
+puntos de pago. ¡Y siempre es seguro!
+● Envío gratis desde $ 2.500
+Solo por estar registrad@ en Mercado Liebre tenés envíos gratis en miles de
+productos. Es un beneficio de Mercado Puntos.
+
+7
+
+● Seguridad, de principio a fin
+¿No te gusta? ¡Devolvelo! En Mercado Liebre, no hay nada que no puedas hacer,
+porque estás siempre protegid@.
+● Copyright © 1999-2020 MercadoLiebre S.R.L.
+
+<form action="/crear-producto" method="POST" class="creat-form">
+            <div class="control-form">
+                <label> Nombre del producto:</label>
+                <input type="text" name="nombre" class="control">
+            </div>
+            <div class="control-form"> 
+                <label> Precio:</label>
+                <input type="text" name="precio" placeholder="120.68" class="control">
+            </div>
+            <div class="control-form">
+            <label> Ciudad</label>
+            <select name="ciudad" class="control">
+                <option value="buenosAires">Buenos Aires</option>
+                <option value="rosario">Rosario</option>
+                <option value="cordoba">Córdoba</option>
+            </select>
+            </div>
+            <div class="control-form">
+                <label> Categoría</label>
+                <select name="categoria" class="control">
+                    <option value="hogar">Hogar</option>
+                    <option value="electro">Electro</option>
+                    <option value="indumentaria">Indumentaria</option>
+                </select>
+                </div>
+                <div class="control-form">
+                    <label> Descripcion</label>
+                <textarea name="descripcion" class="control"></textarea>
+            </div>
+            <div class="control-form">
+                <label> Esta en oferta:</label>
+                <input type="radio" name="enOferta">
+            </div>
+            <button type="reset" class="button-form">Borrar</button>
+            <button type="submit" class="button-form">Guardar</button>
+        
+            
+        </form> 
