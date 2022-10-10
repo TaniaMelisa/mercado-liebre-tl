446736b3eff05f7271320ecc35c1461a9627fd38
TaniaMelisa <taniamelisalopez@gmial.com>
2022-10-10 17:36:19 -0300

    gitignore

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const publicPath= path.resolve(__dirname, "./public")
 app.use(express.static(publicPath))
 
 
-const PORT = process.env.PORT || 3000
-
-
-
-app.listen(PORT, () => console.log('Server on'));
+const port = process.env.PORT || 3001;
+app.listen(port,() => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
