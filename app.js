c8c23a5a99429e49bf9b173b3a5e70f024fdf477
TaniaMelisa <taniamelisalopez@gmial.com>
2022-10-10 12:32:28 -0300

    subo todo listo

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const app = express()
 const publicPath= path.resolve(__dirname, "./public")
 app.use(express.static(publicPath))
 
-const port = process.env.PORT || 3001
 
-app.listen (port, ()=> 
-    console.log("Servidor corriendo en el puerto ${port}`
-))
+const PORT = process.env.PORT || 3000
+
+
+
+app.listen(PORT, () => console.log('Server on'));
 
 app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
